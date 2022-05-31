15ff1aa9895bc9cb2bc8e5065227344b74be965d
shivamkumraa9 <shivamkumraa9@gmail.com>
2022-05-31 17:01:27 +0530

    added port

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DB)
   .then(() => console.log('db connected'))
